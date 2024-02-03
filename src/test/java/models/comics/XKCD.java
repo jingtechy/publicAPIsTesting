5766403606516d626c83efeb417d6d8ebf786804
Jingbin Wang <jingtechy@gmail.com>
2024-02-04 12:20:29 +1300

    add new test

--- src/test/java/models/comics/XKCD.java
+++ src/test/java/models/comics/XKCD.java
@@ -1,2 +1,28 @@
-package models.comics;public class XKCD {
+package models.comics;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class XKCD {
+
+    @JsonProperty("safe_title")
+    private String safeTitle;
+    private String img;
+
+    public String getSafeTitle() {
+        return safeTitle;
+    }
+
+    public void setSafeTitle(String safeTitle) {
+        this.safeTitle = safeTitle;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
 }
