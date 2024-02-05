773f0fa8dc2cffad8a33aecebc016fe3a44e6788
Jingbin Wang <jingtechy@gmail.com>
2024-02-05 16:31:09 +1300

    Add comics test

--- src/test/java/apitests/ComicsTest.java
+++ src/test/java/apitests/ComicsTest.java
@@ -1,2 +1,19 @@
-package apitests;public class ComicsTest {
+package apitests;
+
+import apiresources.ComicsApi;
+import models.comics.XKCD;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ComicsTest {
+
+    @Test
+    public void xkcdComicTest() {
+
+        XKCD xkcd = ComicsApi.getXKCDComicApi();
+
+        assertThat(xkcd.getSafeTitle().equals("Greenhouse Effect")).isTrue();
+        assertThat(xkcd.getImg().equals("https://imgs.xkcd.com/comics/greenhouse_effect.png")).isTrue();
+    }
 }
